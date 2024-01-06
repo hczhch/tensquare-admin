2afc1d569d0a076ae84d9182f24b26488ffb2b8b
Vito <hczhch@ymail.com>
2024-01-06 18:48:00 +0800

    k8s

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"http://192.168.207.131:7300/mock/5c0b42c85b4c7508d4dc568c/1024"'
   //BASE_API: '"http://tensquare-zuul-dev.zhch.lan"' // 管理员网关
-  BASE_API: '"http://tensquare-zuul-headless.k8s-tensquare:10020"'
+  BASE_API: '"http://tensquare-zuul.zhch.lan"'
 }
